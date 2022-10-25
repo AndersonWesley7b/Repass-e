d3b97d62b67c3501656f5484bebfee3e5dbde8a2
Anderson <Anderson@DESKTOP-B5CGT8T>
2022-10-25 16:49:12 -0300

    Criação Contexto da Aplicação

---
+++ Repass-e.Infrastructure/DataContext/AppDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repass_e.Domain.Entities;
+
+namespace Repass_e.Infrastructure.DataContext
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options)
+            :base(options)
+        {}
+        public DbSet<Event> Events { get; set; }
+    }
+}
--- Repass-e.Interface/Program.cs
+++ Repass-e.Interface/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Repass_e.Infrastructure.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AppDataContext>(options =>
+    options.UseInMemoryDatabase("Database"));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
