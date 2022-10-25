e6c386ec6a89049b9fed8c89e5beff14ee203874
Anderson <Anderson@DESKTOP-B5CGT8T>
2022-10-25 17:53:06 -0300

    Inicio Generic Repository

---
+++ Repass-e.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repass_e.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TContext>
+        where TEntity : class
+        where TContext:DbContext
+    {
+        protected readonly TContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(TContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet;
+        }
+        public IQueryable<TEntity> GetAllReadOnly()
+        {
+            return _dbSet.AsNoTracking();
+        }
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public virtual void MassDelete(List<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+        public virtual void Add(TEntity entity)
+        {
+            _dbSet.Add(entity); 
+        }
+    }
+}
